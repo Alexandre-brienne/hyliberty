a3521e7e94ede2448fc6a01f2b822be6ac9e2976
franck1321 <59582397+franck1321@users.noreply.github.com>
2021-11-09 15:24:25 +0100

    Ajout de l'index

--- index.php
+++ index.php
@@ -1,24 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>authentification</title>
-</head>
-<body>
-    <form action="auth.php" method="post">
-        <div>
-            <p>email :</p>
-            <input type="email" name="email" id="email">
-        </div>
-        <div>  
-            <p>mot de passe</p>
-            <input type="password" name="password" id="password">
-        </div>
-
-        <button type="submit">connecter</button>
-    </form>
-</body>
-</html>
+<?php ?>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="style/Dash.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com"> 
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin=""> 
+            <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,300;0,600;1,300&amp;display=swap" rel="stylesheet">
+            <title>HyLiberty</title>
+        </head>
+        <body>
+            <header>
+                <h1 style="margin: 0px;">HyLiberty</h1>
+            </header>
+            <div class="container">
+                <div class="card">
+                    <h4>Nom Prénom</h4>
+                    <h6>Nom du bateau</h6>
+                    <h6>Location</h6>
+                    <p>Téléphone</p>
+                    <div class="carburant">
+                        <progress value="ARemplacer" max="100">Bar 1</progress><br>
+                        <progress value="ARemplacer" max="100">Bar 2</progress>
+                    </div>
+                </div>
+            </div>
+        </body>
+    </html>
